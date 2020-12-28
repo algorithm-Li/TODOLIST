6d3c7604024866046434dee218a8e745cdecb482
Algorithm <algorithm.cpp@outlook.com>
2020-12-28 09:27:31 +0800

    ninth commit

---
+++ app/src/main/java/com/example/todolist/Activity/ScheduleActivity.java
@@ -0,0 +1,13 @@
+package com.example.todolist.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
